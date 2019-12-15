d5c690fa96044f68f5ec03897a4fdefcef687beb
leety1 <58061933+leety1@users.noreply.github.com>
2019-12-15 21:57:08 +0900

    Add files via upload
    화면 부분으로 하려고 작업중인 부분이에여

--- ex2.java
+++ ex2.java
@@ -1,4 +1,43 @@
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
 
-public class ex2 {
+class ex2 extends JFrame{
 
+	public ex2() {
+		
+	setTitle("¿ÃπÃ¡ˆ");
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	setLocationRelativeTo(getOwner());
+
+	Container c = getContentPane();
+	c.setLayout(null);
+	
+	
+	
+	setLayout(null);
+	setContentPane(new MyPanel());
+	setSize(300, 300);
+	setVisible(true);
 }
+	public class MyPanel extends JPanel{
+		public MyPanel() {
+			JButton n = new JButton("hi");
+			
+			add(n);
+			n.setLocation(50,50);
+			
+		}
+public void paintComponent(Graphics g) {
+	super.paintComponents(g);
+	
+Image icon = new ImageIcon("C:\\Users\\Uter\\Desktop/A.jpg").getImage();
+g.drawImage(icon,50,50,100,100,this);
+	 
+	
+}
+	}
+public static void main(String[] args) {
+	new ex2();
+}}
